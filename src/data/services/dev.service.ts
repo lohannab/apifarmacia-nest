f140400565f4ceceb1a49b0324a6c4a5875e7464
Lohanna <lohannabt@gmail.com>
2026-04-16 13:39:46 -0300

    Ajustes finais para deploy

--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 import { Categoria } from "../../categoria/entities/categoria.entity";
 
+
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {
 
-  createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'db_farmacia',
+            entities: [Categoria],
+            synchronize: true,
     };
   }
 }
